81896ab22482a119cf9e0143a3f4d0366f705fc1
pvv <tuhaoweishao@qq.com>
2024-01-18 16:28:03 +0800

    first Commit

---
+++ mini-react-1/App.js
@@ -0,0 +1,4 @@
+import React from "./core/React.js";
+const App = React.createElement("div", { id: "app" }, "成功啦");
+
+export default App;
---
+++ vite-runner/App.jsx
@@ -0,0 +1,5 @@
+import React from "./core/React.js";
+// const App = React.createElement("div", { id: "app" }, "成功啦");
+const App = <div id="app">我丢</div>
+
+export default App;
